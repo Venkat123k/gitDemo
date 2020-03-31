7581263c00127aff01d72b4c10961a3867c0268b
babu <babugari@gmail.com>
2020-03-31 13:14:16 +0530

    This is my Frist Commit

---
+++ src/test/java/hptours/AppiumTest.java
@@ -0,0 +1,16 @@
+package hptours;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeApp() {
+		System.out.println("NativeApp");
+	}
+	@Test
+	public void AndriodApp() {
+		System.out.println("AndriodApp");
+		
+	}
+
+}
---
+++ src/test/java/hptours/RESTApiTest.java
@@ -0,0 +1,16 @@
+package hptours;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+
+	@Test
+	public void PostJira() {
+		System.out.println("PostJira");
+	}
+	@Test
+	public void deleteRecord() {
+		System.out.println("deleteRecord");
+		
+	}
+}
